cbc84f334532b4368ef73f6417c81f4c0ef0fb6b
Vaclav Hala <vaclavhalajr@gmail.com>
2016-05-22 16:27:19 +0200

    DataGen was unfeasibly sluggish when called in loop, invoking json-gen every time, use its built-in batching instead

--- src/test/java/cz/muni/fi/pv243/spatialtracker/DataGen.java
+++ src/test/java/cz/muni/fi/pv243/spatialtracker/DataGen.java
@@ -5,11 +5,16 @@ import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import java.io.*;
 import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import static java.util.regex.Pattern.compile;
 import static javax.ws.rs.core.HttpHeaders.CONTENT_TYPE;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import lombok.extern.slf4j.Slf4j;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 /**
  * Give it template for json-generator ({@code beta.json-generator.com}).
@@ -29,15 +34,21 @@ public class DataGen {
     private static final String GEN_API_URL = "http://beta.json-generator.com/api/templates";
     private static final Pattern GRAB_XSRF_TOKEN = compile("XSRF-TOKEN=([^;]+);");
 
-    public static String generateFrom(InputStream templateStream){
-        StringBuilder template = new StringBuilder();
-        new BufferedReader(new InputStreamReader(templateStream)).lines().forEach(line -> template.append(line));
-        return generateFrom(template.toString());
+    public static String generateFrom(final InputStream templateStream) {
+        return generateFrom(templateStream, 1).get(0);
     }
 
-    public static String generateFrom(String template) {
+    public static String generateFrom(final String template) {
+        return generateFrom(template, 1).get(0);
+    }
+
+    public static List<String> generateFrom(final InputStream templateStream, final int count) {
+        return generateFrom(readStream(templateStream), count);
+    }
+
+    public static List<String> generateFrom(final String template, final int count) {
         try {
-            String body = formatBody(template);
+            String body = formatBody(template, count);
             String xsrfToken = getXsrfToken();
             return generate(body, xsrfToken);
         } catch (Exception e) {
@@ -57,20 +68,32 @@ public class DataGen {
         return new URLDecoder().decode(rawToken, "UTF-8");
     }
 
-    private static String formatBody(String from) {
+    private static String formatBody(String from, int count) {
         String escaped = from.replace("\"", "\\\"")
                              .replace('\n', ' ')
                              .replace('\r', ' ');
-        return String.format("{\"body\":\"%s\"}", escaped);
+        return String.format("{\"body\":\"[{'repeat(%d)':%s}]\"}",
+                             count, escaped);
     }
 
-    private static String generate(String formattedBody, String xsrfToken) throws UnirestException {
-        return Unirest.post("http://beta.json-generator.com/api/templates")
-                      .header("X-XSRF-TOKEN", xsrfToken)
-                      .header(CONTENT_TYPE, APPLICATION_JSON)
-                      .body(formattedBody)
-                      .asJson().getBody().getObject()
-                      .getJSONObject("generatedJSON").toString();
+    private static List<String> generate(String formattedBody, String xsrfToken) throws UnirestException {
+        JSONObject jsonGenResp =
+                Unirest.post("http://beta.json-generator.com/api/templates")
+                       .header("X-XSRF-TOKEN", xsrfToken)
+                       .header(CONTENT_TYPE, APPLICATION_JSON)
+                       .body(formattedBody)
+                       .asJson().getBody().getObject();
+        List<String> items = new ArrayList<>();
+        for (Object item : jsonGenResp.getJSONArray("generatedJSON")) {
+            items.add(item.toString());
+        }
+        return items;
+    }
+
+    private static String readStream(final InputStream in) {
+        StringBuilder template = new StringBuilder();
+        new BufferedReader(new InputStreamReader(in)).lines().forEach(line -> template.append(line));
+        return template.toString();
     }
 
 }
