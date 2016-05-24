e11d0b2deb57cefec8e0ec8f7a9464aeebec97a4
radike <radike@mail.muni.cz>
2016-05-24 07:07:38 -0700

    WebChatService creates a message with the name of the actual user

--- src/main/java/cz/muni/fi/pv243/spatialtracker/webchat/WebChatService.java
+++ src/main/java/cz/muni/fi/pv243/spatialtracker/webchat/WebChatService.java
@@ -7,18 +7,17 @@ import javax.annotation.security.RolesAllowed;
 import javax.ejb.Stateless;
 import javax.enterprise.event.Event;
 import javax.inject.Inject;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
+import cz.muni.fi.pv243.spatialtracker.users.BasicAuthUtils;
 import cz.muni.fi.pv243.spatialtracker.webchat.model.NewWebChatMessage;
 import cz.muni.fi.pv243.spatialtracker.webchat.model.NewWebChatMessageEvent;
 import cz.muni.fi.pv243.spatialtracker.webchat.model.WebChatMessage;
 import cz.muni.fi.pv243.spatialtracker.webchat.store.WebChatMessageStore;
+
+import static cz.muni.fi.pv243.spatialtracker.users.BasicAuthUtils.decodeBasicAuthLogin;
+import static javax.ws.rs.core.HttpHeaders.AUTHORIZATION;
 
 @Path("/messages/{room}")
 @RolesAllowed("USER")
@@ -39,9 +38,10 @@ public class WebChatService {
 
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
-	public void addMessage(NewWebChatMessage newMessage, @PathParam("room") String roomName) {
+	public void addMessage(NewWebChatMessage newMessage, @PathParam("room") String roomName, final @HeaderParam(AUTHORIZATION) String currentUserBasicAuth) {
+        BasicAuthUtils.LoginPass auth = decodeBasicAuthLogin(currentUserBasicAuth);
 		WebChatMessage fullMessage = new WebChatMessage();
-		fullMessage.name("John Doe");//TODO
+		fullMessage.name(auth.login());
 		fullMessage.text(newMessage.text());
 		fullMessage.created(new Date());
 		messages.addMessage(roomName, fullMessage);
