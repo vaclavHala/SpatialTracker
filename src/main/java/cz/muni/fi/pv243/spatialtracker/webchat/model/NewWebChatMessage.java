cdd9b74f255a41b40d4e870b23d557775b16e20e
radike <radike@mail.muni.cz>
2016-06-03 23:36:53 +0200

    Creates WebChatService tests

--- src/main/java/cz/muni/fi/pv243/spatialtracker/webchat/model/NewWebChatMessage.java
+++ src/main/java/cz/muni/fi/pv243/spatialtracker/webchat/model/NewWebChatMessage.java
@@ -2,15 +2,15 @@ package cz.muni.fi.pv243.spatialtracker.webchat.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
 @ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class NewWebChatMessage {
 
 	@NotNull(message = "{webchat.msg.empty}")
